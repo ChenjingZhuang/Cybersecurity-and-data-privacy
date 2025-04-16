22d9e5d6adc9bb06741d7bd3984b0cfed694fa1f
ChenjingZhuang <143402905+ChenjingZhuang@users.noreply.github.com>
2025-04-16 23:32:35 +0300

    Delete phase4/app.js

--- phase4/app.js
+++
@@ -1,8 +0,0 @@
-pageexport function handleRequest(request) {
-    const url = new URL(request.url);
-    const page = url.searchParams.get('page') || 'home';
-    const count = url.searchParams.get('count') || '1';
-  
-    return new Response(`Page: ${page}, Count: ${count}`); 
-  }
-  
